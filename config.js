f526d296482dfb06314ddca6937e9f7b4c03f5b0
fufu <aichaabady0@gmail.com>
2026-05-03 19:47:56 +0200

    Update Firebase API key in config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBwpvOG3MyfQwqfAK4pwf-7TBKNFONIrPU", 
+    authDomain: "mazechat-78945.firebaseapp.com",
+    databaseURL: "https://mazechat-78945-default-rtdb.firebaseio.com",
+    projectId: "mazechat-78945",
+    storageBucket: "mazechat-78945.firebasestorage.app",
+    messagingSenderId: "816965158155",
+    appId: "1:816965158155:web:54ba35107bd86f912b0e0e",
+    measurementId: "G-949E3H88JK"
+};
